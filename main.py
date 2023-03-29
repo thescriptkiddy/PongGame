17a47282c4092cf1c8af256767a78ba4f4004a99
simon.hardt@collyphery.com <s-Q-6qJn@XQi!RBXq@.A>
2023-03-29 14:51:00 +0200

    Created the screen and paddles. Still need to fix some issues.

---
+++ main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+
+# Create the screen
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+# Create and move a paddle
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+screen.exitonclick()
